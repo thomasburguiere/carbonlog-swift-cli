88cd65979101c174f1f0550a04b5003018d71f86
Thomas Burguiere <thomas.burguiere@gmail.com>
2025-02-13 01:49:16 +0100

    feat: dependency

--- Package.swift
+++ Package.swift
@@ -6,14 +6,18 @@ import PackageDescription
 let package = Package(
   name: "carbonCLI",
   dependencies: [
-    .package(url: "https://github.com/thomasburguiere/carbonlog-swift-lib", branch: "master")
+    .package(url: "https://github.com/thomasburguiere/carbonlog-swift-lib", branch: "master"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
       name: "carbonCLI",
-      dependencies: [.product(name: "CarbonLogLib", package: "carbonlog-swift-lib")]
+      dependencies: [
+        .product(name: "CarbonLogLib", package: "carbonlog-swift-lib"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ]
     )
   ]
 )
