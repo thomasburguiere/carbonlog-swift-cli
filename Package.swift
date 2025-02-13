368f22f1da18ce8644e8d04a70a9bf521aad6e24
Thomas Burguiere <thomas.burguiere1@swisscom.com>
2025-02-13 01:46:28 +0100

    feat: import carbon lib, redefine entry point

--- Package.swift
+++ Package.swift
@@ -5,9 +5,15 @@ import PackageDescription
 
 let package = Package(
   name: "carbonCLI",
+  dependencies: [
+    .package(url: "https://github.com/thomasburguiere/carbonlog-swift-lib", branch: "master")
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
-    .executableTarget(name: "carbonCLI")
+    .executableTarget(
+      name: "carbonCLI",
+      dependencies: [.product(name: "CarbonLogLib", package: "carbonlog-swift-lib")]
+    )
   ]
 )
---
+++ Sources/carbon.swift
@@ -0,0 +1,8 @@
+import CarbonLogLib
+
+@main
+struct CarbonCli {
+  static func main() {
+    print("Hello Carbon")
+  }
+}
